1fb4126a10d41bbc3b4a0b4a083d95154c04ad8f
nfmorenog <nfmorenog@unal.edu.co>
2020-04-13 22:14:02 -0500

    Spring initializer
    mongo, postgres, jpa, websocket

---
+++ src/main/java/com/as2020/chat/ChatMsApplication.java
@@ -0,0 +1,13 @@
+package com.as2020.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/as2020/chat/ChatMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as2020.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
